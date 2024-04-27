51474dd6877d5ab724eb819389ea5307a70fc690
jeanmbusa <jeanmbusa@mbusajean41@gmail.com>
2024-04-27 08:47:32 +0200

    first commit

---
+++ new 1.js
@@ -0,0 +1,17 @@
+<!doctype html>
+	<htlm>
+		<head>
+			<title> variable en javascript</title>
+			<meta charset="utf-8"> 
+		</head>
+		<body>
+			<script>
+				<h1> VARIABLE EN JAVASCRIPT</h1>
+					var x=10, y=45, z=23;
+					x=x - 5;
+					y=y + 5;
+					z=z / 2;
+					
+			</script>
+		</body>
+	</htlm>
